5838796b1b230b10a328068cb665924d41285d7a
Ubuntu <ubuntu@ip-172-31-40-3.eu-north-1.compute.internal>
2025-09-10 13:08:31 +0000

    Added docker deployment files and fixed path issue

--- api/app.py
+++ api/app.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import joblib
+import os
 from pydantic import BaseModel
 from fastapi import FastAPI
 
+script_dir = os.path.dirname(__file__)
+
+model_relative_path = os.path.join(script_dir, '../models/stock_model.pkl')
+
+model, scaler, features_cols = joblib.load(model_relative_path)
+
 model_path = "../models/stock_model.pkl"
 
-model, scaler, features_cols = joblib.load(model_path)
+#model, scaler, features_cols = joblib.load(model_path)
 
 app = FastAPI()
 
@@ -18,3 +25,8 @@ def predict_stock(data: StockData):
     df_scaled = scaler.transform(df)
     predicted = model.predict(df_scaled)[0]
     return {"prediction": "up" if predicted == 1 else "down"}
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello"}
